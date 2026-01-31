9f7f348d53c93631ac3023df6084fb16258f716b
Toxirov36 <dilshodbektohirov40@gmail.com>
2026-01-31 23:06:01 +0500

    first commit

--- src/validators/permissionValidator.js
+++ src/validators/permissionValidator.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 const permissionSchema = Joi.object({
-  user: Joi.string().required(),  // ObjectId string
+  user: Joi.string().required(),
   permissionModel: Joi.string().required(),
   actions: Joi.array().items(Joi.string().valid('create', 'read', 'update', 'delete')).required()
 });
