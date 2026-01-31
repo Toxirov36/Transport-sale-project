b9b13f910175001c514e5d0beec9ed6a9a84dbe6
Toxirov36 <dilshodbektohirov40@gmail.com>
2026-01-31 23:02:04 +0500

    staff fayl yangilandi"

--- src/models/staff.js
+++ src/models/staff.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const staffSchema = new mongoose.Schema({
-  // branch: { type: mongoose.Schema.Types.ObjectId, ref: 'Branch', required: true },
+  branch: { type: mongoose.Schema.Types.ObjectId, ref: 'Branch', required: true },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
