895fdee3ed25c54e6589d331a97054723bd7ac2f
Toxirov36 <dilshodbektohirov40@gmail.com>
2026-01-31 23:07:17 +0500

    first commit

--- src/models/otp.js
+++ src/models/otp.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const otpSchema = new mongoose.Schema({
   email: { type: String, required: true },
   otp: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: 180 } // 180 soniya (3 daqiqa) dan keyin o'chib ketadi
+  createdAt: { type: Date, default: Date.now, expires: 180 }
 });
 
 export default mongoose.model('Otp', otpSchema);
